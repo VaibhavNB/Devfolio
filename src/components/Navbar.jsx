ce6622bf65a21108c4fd2ae5c0a6c8d79aa58f04
VaibhavNB <badigervaibhav8@gmail.com>
2025-07-31 22:14:41 +0530

    Initial Commit

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,54 @@
+import React, { useEffect, useState } from 'react'
+import { navLinks } from '../constants'
+
+const Navbar = () => {
+
+  const [scrolled, setScrolled] = useState(false);
+
+  useEffect(()=>{
+    const handleScroll = ()=>{
+      const isScrolled = window.screenY > 10;
+      setScrolled(true);
+    }
+
+    window.addEventListener('scroll', handleScroll);
+
+    return () => window.removeEventListener('scroll', handleScroll);
+  },[])
+
+  return (
+    <header className={`navbar ${scrolled? 'scrolled' : 'not-scrolled' }`}>
+      <div className="inner">
+        <a className='logo' href="#hero">
+          Vaibhav NB | FSD
+        </a>
+
+        <nav className='desktop'>
+          <ul>
+            {
+              navLinks.map(({link, name})=>(
+                <li className='group' key={name}>
+                  <a href={link}>
+                    <span>
+                      {name}
+                    </span>
+                    <span className='underline' />
+                  </a>
+                </li>
+              ))
+            }
+          </ul>
+        </nav>
+        <a className='contact-btn group' href="#contact">
+          <div className="inner">
+            <span>
+              Contact me
+            </span>
+          </div>
+        </a>
+      </div>
+    </header>
+  )
+}
+
+export default Navbar
